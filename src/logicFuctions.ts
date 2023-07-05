0f69a9b02f230c5db2adcfdb387e94412a291fb9
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-07-05 16:58:31 +0000

    optimization: move functions to a separate .ts file "logicFunctions"

---
+++ src/logicFuctions.ts
@@ -0,0 +1,63 @@
+import * as vscode from "vscode";
+import { exec } from "child_process";
+import path = require("path");
+// Compilation logic
+export async function compileAssembly() {
+    const editor = vscode.window.activeTextEditor;		// get the active editor
+
+    // check and stop compilation if there is no active editor
+    if (!editor) {
+        vscode.window.showErrorMessage('No active text editor.');
+        return;
+    }
+
+    const assemblyFilePath = editor.document.fileName;	// get the path to the assembly file
+    const objectFilePath = assemblyFilePath.replace(/\.asm$/, '.o'); // Replace .asm with .o for the object file
+    const basenamePath = assemblyFilePath.replace(/\.asm$/, "");	// get the basefilepath from the .asm filepath
+
+    const nasmCommand = `nasm -f elf64 -o ${objectFilePath} ${assemblyFilePath}`;	// nasm command to compile the .asm to object code .o
+    const ldCommand = `ld "${objectFilePath}" -o "${basenamePath}"`;				// linking command to create an executable file
+
+    // run the nasmCommand
+    exec(nasmCommand, (nasmError, nasmStdout, nasmStderr) => {
+        // check if error in compiling and stop compilation
+        if (nasmError) {
+            vscode.window.showErrorMessage('Compilation failed. Check the output for errors.');
+            return;
+        } else {
+            exec(ldCommand, (ldError, ldStdout, ldStderr) => {
+                // check if there's is an error in linking and stop the compilation
+                if (ldError) {
+                    vscode.window.showErrorMessage('Linking failed. Check the output for errors.');
+                } else {
+                    vscode.window.showInformationMessage('Compilation and linking successful.');
+                }
+                console.log(ldStdout);
+                console.log(ldStderr);
+            });
+            console.log(nasmStdout);
+            console.log(nasmStderr);
+        }
+    });
+    return path.basename(assemblyFilePath, path.extname(assemblyFilePath)); // return the basename of the file
+}
+
+// Run logic
+export async function runAssembly() {
+    const fileBasename = await compileAssembly(); // the compileAssembly() command returns the basename of the assembler file
+
+    const execCommand = `./${fileBasename}`; // command to execute the executable
+
+    // check and get a terminal with the name "NASM Terminal"
+    const existingTerminal = vscode.window.terminals.find((terminal) => terminal.name === 'NASM Terminal');
+
+    if (existingTerminal) {
+        existingTerminal.sendText(execCommand); // send the execCommand to it if such exists
+    } else {
+        // else create a new terminal with the name "NASM Terminal" and send the execCommand
+        const terminal = vscode.window.createTerminal('NASM Terminal');
+        terminal.sendText(execCommand);
+        terminal.show();
+    }
+
+}
